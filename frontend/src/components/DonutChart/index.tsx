9f14213c1a0996f2dcb4753eee56b12c57cbd4e6
Matheus de Assis <assis4002@gmail.com>
2021-05-08 07:53:27 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale'
 import { BASE_URL } from 'utils/requests'
@@ -10,7 +11,18 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    let chartData: ChartData = { labels: [], series: [] }
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[]
+                const myLabels = data.map(x => x.sellerName)
+                const mySeries = data.map(x => x.sum)
+
+                setChartData({ labels: myLabels, series: mySeries })
+            })
+    }, [])
 
     axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then(response => {
@@ -18,11 +30,11 @@ const DonutChart = () => {
             const myLabels = data.map(x => x.sellerName)
             const mySeries = data.map(x => x.sum)
 
-            chartData = { labels: myLabels, series: mySeries }
+            setChartData({ labels: myLabels, series: mySeries })
 
                 console.log(response.data)
         })
-        
+
     const options = {
         legend: {
             show: true
